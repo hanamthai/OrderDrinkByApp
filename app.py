31e240fe1a5373168726bcf4f5d3367b4d8e7caf
ThaiNamHa-windows <hanamthai02@gmail.com>
2023-03-21 15:37:51 +0700

    format order date and display basic information of order

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ from flask_jwt_extended import jwt_required
 from flask_jwt_extended import JWTManager
 from flask_jwt_extended import get_jwt
 
+from format_timestamp import format_timestamp as ft
 
 
 app = Flask(__name__)
@@ -40,17 +41,17 @@ mail = Mail(app)
 CORS(app)   # Cross-origin resource sharing
 
 # Local
-# DB_HOST = "localhost"
-# DB_NAME = "Drink Order"
-# DB_USER = "postgres"
-# DB_PASS = "123"
+DB_HOST = "localhost"
+DB_NAME = "Drink Order"
+DB_USER = "postgres"
+DB_PASS = "123"
 
 
 # Public
-DB_HOST = "postgresql-hanamthai.alwaysdata.net"
-DB_NAME = "hanamthai_drinkorder"
-DB_USER = "hanamthai_admin"
-DB_PASS = "021101054"
+# DB_HOST = "postgresql-hanamthai.alwaysdata.net"
+# DB_NAME = "hanamthai_drinkorder"
+# DB_USER = "hanamthai_admin"
+# DB_PASS = "021101054"
 
 conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER,
                         password=DB_PASS, host=DB_HOST)
@@ -632,18 +633,31 @@ def userConfirmCompletedOrder():
 
 
 # user view order history
-# @app.route('/order/history', methods = ['GET'])
-# @jwt_required()
-# def userOrderHistory():
-#     userid = get_jwt_identity()
+@app.route('/order/history', methods = ['GET'])
+@jwt_required()
+def userOrderHistory():
+    userid = get_jwt_identity()
 
-#     cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
     
-#     sql_history = """
+    sql_history = """
+    SELECT 
+        status,address,orderdate,totalprice
+    FROM orders
+    WHERE 
+        userid = %s 
+            AND 
+        (status = 'Completed' OR status = 'Cancelled')
+    ORDER BY orderdate
+    """
+    sql_where = (userid,)
+    cursor.execute(sql_history,sql_where)
+    row = cursor.fetchall()
+    data = [{"status":i["status"],"address":i["address"],"orderdate":ft(str(i["orderdate"])),"totalprice":i["totalprice"]} for i in row]
     
-#     """
-    
-#     return jsonify({"message":"OK"})
+    resp = jsonify(data=data)
+    resp.status_code = 200
+    return resp
 
 
 
